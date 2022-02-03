d6c4cb8f00ec83615b997479f86572fe6aacb849
enjinerd <dev@ron.my.id>
2022-02-03 12:45:34 +0700

    :zap: perf(app): change husky and rm unused dir

--- src/__tests__/NotFound.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { NotFound } from 'pages';
-
-test('Renders NotFound page correctly', async () => {
-  render(<NotFound />);
-  const notFoundAlert = await screen.queryByText(/NotFound/);
-  expect(notFoundAlert).toBeInTheDocument();
-});
