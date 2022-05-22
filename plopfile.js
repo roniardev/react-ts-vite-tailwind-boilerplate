db4e7f5a537a1f1d55873dfe84f9d176f73e079f
Roni Ardiyanto <35429278+enjinerd@users.noreply.github.com>
2022-05-22 13:40:42 +0000

    :truck: chore(app): add plop js to generate component

---
+++ plopfile.js
@@ -0,0 +1,34 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'React component using Typescript',
+    prompts: [
+      {
+        type: 'input',
+        name: 'dir',
+        message: 'Directory: ',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: ',
+      },
+      {
+        type: 'input',
+        name: 'tag',
+        message: 'Tag name: ',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{dir}}/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/component/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{dir}}/__test__/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/component/Component.test.tsx.hbs',
+      },
+    ],
+  });
+};
