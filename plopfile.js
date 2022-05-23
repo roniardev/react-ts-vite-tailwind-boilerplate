5d424e6c86db4610bcfa3277cef09800fa1f0f03
Roni Ardiyanto <35429278+enjinerd@users.noreply.github.com>
2022-05-23 03:36:29 +0000

    :truck: chore(app): add plop to generate page

--- plopfile.js
+++ plopfile.js
@@ -26,8 +26,59 @@ module.exports = function (plop) {
       },
       {
         type: 'add',
-        path: 'src/components/{{dir}}/__test__/{{pascalCase name}}.test.tsx',
+        path: 'src/components/{{dir}}/__tests__/{{pascalCase name}}.test.tsx',
         templateFile: 'templates/component/Component.test.tsx.hbs',
+      },
+    ],
+  });
+  plop.setGenerator('page', {
+    description: 'React page using Typescript',
+    prompts: [
+      {
+        type: 'input',
+        name: 'dir',
+        message: 'Directory: ',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: ',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/pages/{{dir}}/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/page/Page.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/pages/{{dir}}/__tests__/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/page/Page.test.tsx.hbs',
+      },
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/pages/index.ts',
+        // Pattern tells plop where in the file to inject the template
+        pattern: `/* PLOP_INJECT_IMPORT */`,
+        template: `export * from './{{dir}}/{{pascalCase name}}';`,
+      },
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/routes/Router.tsx',
+        // Pattern tells plop where in the file to inject the template
+        pattern: `/* PLOP_INJECT_IMPORT */`,
+        template: `,{{pascalCase name}}`,
+      },
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/routes/Router.tsx',
+        // Pattern tells plop where in the file to inject the template
+        pattern: `/* PLOP_ROUTE_INJECT_IMPORT */`,
+        template: `<Route path="/{{lowerCase name}}" element={<{{name}} />} />`,
       },
     ],
   });
